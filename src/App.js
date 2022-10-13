6548c117e49886be26247bc07d5d4d67f80ca01f
Alexis Mora <alexis.mora1v@gmail.com>
2022-10-12 21:57:01 -0500

    Buscador de imagenes basico
    Agregada conexión con la API.
    Agregado el contenedor con las imagenes.
    Agregada la presentación de las iamgenes.
    Agregado buscador.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import { useState } from "react"
+import { Field, Form, Formik } from "formik"
+import './header.css'
+import './content.css'
+import './article.css'
+
 const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+  console.log(photos)
   return (
     <div>
       <header>
-        
+        <Formik
+        initialValues={{ search: '' }}
+        onSubmit={async values => {
+          // Llamar a la API
+          const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+          {
+            headers: {
+              'Authorization': 'Client-ID nTihTOELMzpv0Sabwjz1ySq3zCPRfq-JkCn3fKVWH7o'
+            }
+          })
+          const data = await response.json()
+          setPhotos(data.results)
+        }}
+        >
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo => 
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} />
+              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+            </article>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
